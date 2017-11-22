bfbf7c4d4fa7a4f898e5af0d5fe5744da0adcc30
Wali Rahman <wali.rahman89@yahoo.com>
2017-11-22 13:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,3 +23,10 @@ function exactMatch (drivers, matcher) {
     return matches;
   });
 }
+
+function exactMatchToList (drivers, matcher) {
+  return exactMatch (drivers, matcher)
+  .map(function (driver){
+    return driver.name
+  })
+}
